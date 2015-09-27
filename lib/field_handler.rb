ff13b5b0c721b624a287a0c8bb9de6b84ec206f4
Michael Clarke <michaelclarkecs@gmail.com>
2015-09-27 10:03:07 +0100

    Refactor field handler into a class

---
+++ lib/field_handler.rb
@@ -0,0 +1,28 @@
+require_relative 'types/normal'
+require_relative 'types/value'
+require_relative 'types/group'
+require_relative 'types/weighted'
+
+
+class FieldHandler
+  def handle(parent, field)
+    field_name = field[0]
+    field_options = field[1]
+
+    handlers = {}
+
+    handlers['normal'] = Normal.new
+    handlers['value'] = Value.new
+    handlers['group'] = Group.new
+    handlers['weighted'] = Weighted.new
+
+    field_type = field_options.keys[0]
+    if handlers.keys.include? field_type then
+      field_value = handlers[field_type].handle(field_options[field_type])
+    else
+      raise "Unsupported type #{field_type}"
+    end
+
+    parent[field_name] = field_value
+  end
+end
--- lib/types/group.rb
+++ lib/types/group.rb
@@ -1,10 +1,12 @@
-require_relative '../field'
+require_relative '../field_handler'
 
 class Group
   def handle(options)
+    field_handler = FieldHandler.new
+
     hash = {}
     options.each do |field|
-      handle_field hash, field
+      field_handler.handle hash, field
     end
     hash
   end
