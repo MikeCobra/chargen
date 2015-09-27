6a08c4f7df30ac071a517bde6c9edbfed6a10b45
Michael Clarke <michaelclarkecs@gmail.com>
2015-09-27 18:20:55 +0100

    Implement tests for weighted type

--- spec/types/weighted_spec.rb
+++ spec/types/weighted_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../../lib/types/weighted'
+require 'pickup'
 
 RSpec.describe Weighted, "#handle" do
+  it "should set up the pickup and return the picked value" do
+    pickup = double 'Pickup'
+    options = { 'Option' => 100 }
+    allow(Pickup).to receive(:new).with(options).and_return(pickup)
+    weighted = Weighted.new
+
+    expect(pickup).to receive(:pick).and_return('Option')
+
+    result = weighted.handle(options)
+
+    expect(result).to eq 'Option'
+  end
 end
