80d6972744cc9f59be72d8fdee2d66b9d10f1769
Michael Clarke <michaelclarkecs@gmail.com>
2015-09-27 07:31:45 +0100

    Add support for min and max values in the normal distribution

--- lib/types/normal.rb
+++ lib/types/normal.rb
@@ -1,13 +1,28 @@
 require 'simple-random'
 
 class Normal
-  def handle(options)
-    sr = SimpleRandom.new
-    sr.set_seed
+  def initialize
+      @random_generator = SimpleRandom.new
+      @random_generator.set_seed
+  end
 
+  def handle(options)
     mean = options['mean']
     std_dev = options['std_dev']
 
-    sr.normal(mean, std_dev).round
+    min = options['min']
+    max = options['max']
+
+    value = @random_generator.normal(mean, std_dev).round
+
+    if min and value < min then
+      value = min
+    end
+
+    if max and value > max then
+      value = max
+    end
+
+    value
   end
 end
