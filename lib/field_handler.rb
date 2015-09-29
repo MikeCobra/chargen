b488ba2d567676b7c056b0ce52b39c3c7219f3f4
Michael Clarke <michaelclarkecs@gmail.com>
2015-09-29 12:40:29 +0100

    Separate out field handler logic

--- lib/field_handler.rb
+++ lib/field_handler.rb
@@ -8,20 +8,31 @@ class FieldHandler
     field_name = field[0]
     field_options = field[1]
 
-    handlers = {}
-
-    handlers['normal'] = Normal.new
-    handlers['value'] = Value.new
-    handlers['group'] = Group.new
-    handlers['weighted'] = Weighted.new
-
     field_type = field_options.keys[0]
-    if handlers.keys.include? field_type
-      field_value = handlers[field_type].handle(field_options[field_type])
+
+    handler = select_handler(field_type)
+
+    parent[field_name] = handler.handle(field_options[field_type])
+  end
+
+  private
+
+  def select_handler(field_type)
+    !@handlers && setup_handlers
+
+    if @handlers.keys.include? field_type
+      @handlers[field_type]
     else
       fail "Unsupported type #{field_type}"
     end
+  end
 
-    parent[field_name] = field_value
+  def setup_handlers
+    @handlers = {}
+
+    @handlers['normal'] = Normal.new
+    @handlers['value'] = Value.new
+    @handlers['group'] = Group.new
+    @handlers['weighted'] = Weighted.new
   end
 end
