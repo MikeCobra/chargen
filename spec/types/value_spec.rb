7d10732b01a243b5f96b35d5b62973ccc09bf62d
Michael Clarke <michaelclarkecs@gmail.com>
2015-09-29 16:39:15 +0100

    Refactor tests for consistency

--- spec/types/value_spec.rb
+++ spec/types/value_spec.rb
@@ -1,9 +1,12 @@
 require_relative '../../lib/types/value'
 
 RSpec.describe Value, "#handle" do
+  before(:each) do
+    @value = Value.new
+  end
+
   it "returns the options" do
-    value = Value.new
-    result = value.handle(12)
+    result = @value.handle(12)
     expect(result).to eq 12
   end
 end
--- spec/types/weighted_spec.rb
+++ spec/types/weighted_spec.rb
@@ -2,15 +2,18 @@ require_relative '../../lib/types/weighted'
 require 'pickup'
 
 RSpec.describe Weighted, "#handle" do
+  before(:each) do
+    @pickup = double 'Pickup'
+    @weighted = Weighted.new
+  end
+
   it "should set up the pickup and return the picked value" do
-    pickup = double 'Pickup'
     options = { 'Option' => 100 }
-    allow(Pickup).to receive(:new).with(options).and_return(pickup)
-    weighted = Weighted.new
+    allow(Pickup).to receive(:new).with(options).and_return(@pickup)
 
-    expect(pickup).to receive(:pick).and_return('Option')
+    expect(@pickup).to receive(:pick).and_return('Option')
 
-    result = weighted.handle(options)
+    result = @weighted.handle(options)
 
     expect(result).to eq 'Option'
   end
